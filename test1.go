26f8a5ad7bd5a1f0eaa861156e03d576663cb692
Bharath Dasaraju <bharath@hooq.tv>
2019-08-14 09:25:52 +0700

    print the current time using the time package

--- test1.go
+++ test1.go
@@ -4,14 +4,17 @@ package main
 // without repeating the "import" keyword.
 import (
 	"fmt"
+	"time"
 )
 /*
 The main function, when part of the main package,
 identifies the entry point of an application.
  */
 
-func main()  {
+func main() {
 	fmt.Println("Hello This is my first GO program")
 //	print(os.Hostname())
+    fmt.Println("The time is now is", time.Now())
+//	fmt.Println("The hostname of your machine is", os.Hostname())
 
 }
